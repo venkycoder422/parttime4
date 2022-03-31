c74a910ae41ec5942185380fe50771999f95fea4
venkycoder <dommaravenkatesulu22@gmail.com>
2022-03-31 18:05:10 +0530

    commiting

---
+++ pw4/higherorder2.js
@@ -0,0 +1,10 @@
+var arr=["Masai","School","Full stack","development"];
+
+var out=arr.map(function(element,i,array){
+    
+        return element[0];
+    
+});
+
+console.log(out);
+//console.log(out);
