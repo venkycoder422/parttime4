4b1ac7f2fe6bbd8634d3f1cfc10b07d7c73e623e
venky422 <dommaravenkatesulu22@gmail.com>
2022-08-22 12:31:09 +0530

    students-queries

--- pw4/U7_2/mongo-queries/employee-salary.js
+++ pw4/U7_2/mongo-queries/employee-salary.js
@@ -1,7 +1,7 @@
 db.employee_salary.find({ department: "Engineering" }).count();
-db.employee_salary.find({ gender: "Female", department: "Engineering" }, { salary: { $lt: 1000000 } }).count()
+db.employee_salary.find({ gender: "Female", department: "Engineering" ,  salary: { $lt: 1000000 } }).count()
 db.employee_salary.find({ salary: { $lt: 80000 } }).count();
-db.employee_salary.find({ department: "Accounting" ,department:"Legal"}, { salary: { $lt: 1000000 } }).count();
+db.employee_salary.find({ department: "Accounting" ,department:"Legal",  salary: { $lt: 1000000 } }).count();
 db.employee_salary.find({ gender: "Male" }).sort({ salary: -1 }).limit(10);
 db.employee_salary.find({ gender: "Female" }).sort({ salary: 1 }).limit(10);
 db.employee_salary.find({ department: "Engineering" }).sort({ salary: -1 }).limit(5);
---
+++ pw4/U7_2/mongo-queries/students-marks.js
@@ -0,0 +1,12 @@
+db.students_marks.find({ gender: "Female" }).count();
+db.students_marks.find({ gender: "Male", $and: [{ maths: { $gt: 85 } }, { science: { $gt: 85 } }, { english: { $gt: 85 } }] }).count();
+db.students_marks.find({ maths: { $gt: 50, $lt: 75 }, english: { $gt: 50, $lt: 75 } });
+db.students_marks.find({ class: "X", section: "A", maths: { $lt: 25 }, science: { $lt: 25 }, english: { $lt: 25 } }).count();
+db.students_marks.find({ class: "V" }).sort({ maths: -1 }).limit(3);
+db.students_marks.find({ class: "I" }).sort({ science: 1 }).limit(5);
+db.students_marks.find({ section: "A", maths: { $lt: 50 }, science: { $lt: 50 }, english: { $lt: 50 } });
+db.students_marks.find({ section: "C", maths: { $gt: 75 }, science: { $gt: 75 }, english: { $gt: 75 } });
+db.students_marks.find().sort({ maths: 1 }).skip(2).limit(10);
+db.students_marks.find().sort({ science: -1 }).skip(4).limit(20);
+db.students_marks.find({ gender: "Female" }).sort({ science: 1, maths: 1 }).skip(3).limit(5);
+db.students_marks.find({ gender: "Male" }).sort({ science: -1, english: -1 }).skip(2).limit(15);
